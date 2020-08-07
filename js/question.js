0b9dd2b6c9155adc8d7027f1fc16f8c097c4d01c
Neelesh Ramane <59531944+neeleshramane@users.noreply.github.com>
2020-08-07 10:47:28 +0530

    111

---
+++ js/question.js
@@ -0,0 +1,79 @@
+
+const quiz = [
+
+    
+    {
+        q: 'Which month has 28 days?',
+        options: ['July','February','March','April'],
+        answer:1
+    },
+
+    {
+        q: 'Which month has 31 days?',
+        options: ['July','February','June','April'],
+        answer:0
+    },
+
+    {
+        q: '2 + 2 = 5 ?',
+        options: ['True','False'],
+        answer:1
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    },
+    {
+        q: 'Which month has 31 days?',
+        options: ['July','February','June','April'],
+        answer:0
+    },
+
+    {
+        q: '2 + 2 = 5 ?',
+        options: ['True','False'],
+        answer:1
+    },
+    {
+        q: '2 + 2 = 5 ?',
+        options: ['True','False'],
+        answer:1
+    },
+    {
+        q: 'Which is what?',
+        options: ['y','t','w','o'],
+        answer:3
+    }
+
+]
